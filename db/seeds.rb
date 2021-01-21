151000bb1b5c346eae86a7b56f649424ec27f0b8
samuel <samuelalmaoherrera@gmail.com>
2021-01-21 18:16:40 -0300

    add a seed for data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-users = User.create([{ name: 'Micronaut' }, { name: 'User' }],[{name: 'Admin'}])
-groups = Group.create([{ name: 'Exercising', icon:'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic3.businessinsider.com%2Fimage%2F57a0b934dd08956a398b48a2-1200%2Fpeople-are-exercising-more-and-eating-healthier.jpg'}])
+users = User.create([{
+        name: 'Micronaut'
+    },
+    { 
+        name: 'User' 
+    },
+    {
+        name: 'Admin'
+    }])
+
+groups = Group.create([{ 
+    name: 'Exercising',
+    icon:'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic3.businessinsider.com%2Fimage%2F57a0b934dd08956a398b48a2-1200%2Fpeople-are-exercising-more-and-eating-healthier.jpg',
+    user_id: 1
+},
+{
+    name: 'Studying',
+    icon: 'http://www.heysigmund.com/wp-content/uploads/Studying-9-Scientifically-Proven-Ways-to-Supercharge-Your-Learning.jpg',
+    user_id: 1
+
+},
+{
+    name: 'Meditating',
+    icon: 'https://static.parade.com/wp-content/uploads/2016/12/meditating-woman-ftr.jpg',
+    user_id: 1
+},
+{
+    name: 'Working',
+    icon: 'https://picjumbo.com/wp-content/uploads/business-woman-working-on-laptop-in-her-office-2210x1473.jpg',
+    user_id: 1
+}])
+
+routines = Routine.create([{
+
+}])
