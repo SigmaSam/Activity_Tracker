fabcc2f8ca44bfbb264e1bcd4fd30d6de86003ee
samuel <samuelalmaoherrera@gmail.com>
2021-01-21 21:19:57 -0300

    add the view for a external routine form.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,26 +15,26 @@ users = User.create([{
         name: 'Admin'
     }])
 
-groups = Group.create([{ 
+groups = Group.create([{
+    name: 'External'
+},
+{
+
     name: 'Exercising',
-    icon:'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic3.businessinsider.com%2Fimage%2F57a0b934dd08956a398b48a2-1200%2Fpeople-are-exercising-more-and-eating-healthier.jpg',
-    user_id: 1
+    icon:'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic3.businessinsider.com%2Fimage%2F57a0b934dd08956a398b48a2-1200%2Fpeople-are-exercising-more-and-eating-healthier.jpg'
 },
 {
     name: 'Studying',
-    icon: 'http://www.heysigmund.com/wp-content/uploads/Studying-9-Scientifically-Proven-Ways-to-Supercharge-Your-Learning.jpg',
-    user_id: 1
+    icon: 'http://www.heysigmund.com/wp-content/uploads/Studying-9-Scientifically-Proven-Ways-to-Supercharge-Your-Learning.jpg'
 
 },
 {
     name: 'Meditating',
-    icon: 'https://static.parade.com/wp-content/uploads/2016/12/meditating-woman-ftr.jpg',
-    user_id: 1
+    icon: 'https://static.parade.com/wp-content/uploads/2016/12/meditating-woman-ftr.jpg'
 },
 {
     name: 'Working',
-    icon: 'https://picjumbo.com/wp-content/uploads/business-woman-working-on-laptop-in-her-office-2210x1473.jpg',
-    user_id: 1
+    icon: 'https://picjumbo.com/wp-content/uploads/business-woman-working-on-laptop-in-her-office-2210x1473.jpg'
 }])
 
 routines = Routine.create([{
